2aee3328f3e3d56bdd33caf739dc22b32887aab5
Yamiku <yamiku2333@gmail.com>
2017-04-13 21:11:18 +0800

    完成addNote，getAllCourses方法

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,56 +21,6 @@ Page({
 
   onLoad: function () {
 
-    // 查找16移动应用的所有课程并打印
-    // BmobUser.getAll('Classes', function (res) {
-    //   for (var i = 0; i < res.length; i++) {
-    //     var data = res[i].attributes;
-    //     if (data.Name == "16语文教育1班") {
-    //       BmobUser.getOneById('Courses', data.CoursesId, function (res2) {
-    //         console.log(res2.attributes);
-    //       });
-    //     }
-    //   }
-    // });
-
-    BmobUser.add('Courses', {
-      Data: [
-        { CourseName: "毛泽东思想和中国特色社会主义理论体系概论", Teacher: "李梓烽", Room: "阶梯B211室", Weeks: "1-18", Day: "1", Start: "1", End: "2" },
-        { CourseName: "高职实用英语(文科)", Teacher: "Prentice Alston", Room: "教B201", Weeks: "9-11", Day: "1", Start: "3", End: "4" },
-        { CourseName: "高职实用英语(文科)", Teacher: "黄启汉", Room: "教B201", Weeks: "1-8", Day: "1", Start: "3", End: "4" },
-        { CourseName: "高职实用英语(文科)", Teacher: "黄启汉", Room: "教B201", Weeks: "12-18", Day: "1", Start: "3", End: "4" },
-        { CourseName: "中国现当代文学", Teacher: "胡勇", Room: "教B109", Weeks: "13-13", Day: "1", Start: "5", End: "6" },
-        { CourseName: "现代汉语", Teacher: "陈珊", Room: "教B209", Weeks: "1-18", Day: "1", Start: "7", End: "8" },
-        { CourseName: "高职应用数学3", Teacher: "秦晓林", Room: "教B209", Weeks: "1-18", Day: "2", Start: "1", End: "2" },
-        { CourseName: "中国现当代文学", Teacher: "胡勇", Room: "主楼103", Weeks: "1-7", Day: "2", Start: "3", End: "4" },
-        { CourseName: "中国现当代文学", Teacher: "胡勇", Room: "主楼103", Weeks: "9-18", Day: "2", Start: "3", End: "4" },
-        { CourseName: "教育学", Teacher: "陈龙图", Room: "阶梯B210室", Weeks: "18-18", Day: "2", Start: "7", End: "7" },
-        { CourseName: "教育学", Teacher: "陈龙图", Room: "阶梯B210室", Weeks: "14-17", Day: "2", Start: "7", End: "8" },
-        { CourseName: "五邑侨乡文化概论(创业精神)", Teacher: "李梓烽", Room: "阶梯B211室", Weeks: "16-18", Day: "2", Start: "10", End: "11" },
-        { CourseName: "基础写作", Teacher: "覃碧卿", Room: "教B206", Weeks: "1-18", Day: "3", Start: "1", End: "2" },
-        { CourseName: "中国古代文学", Teacher: "王洪生", Room: "教B203", Weeks: "1-18", Day: "3", Start: "3", End: "4" },
-        { CourseName: "毛泽东思想和中国特色社会主义理论体系概论", Teacher: "李梓烽", Room: "阶梯B111室", Weeks: "2-2", Day: "3", Start: "5", End: "6" },
-        { CourseName: "计算机应用基础", Teacher: "叶振安", Room: "电脑7室(实A504)", Weeks: "3-6", Day: "3", Start: "10", End: "12" },
-        { CourseName: "计算机应用基础", Teacher: "叶振安", Room: "图403(电脑15室)", Weeks: "1-2", Day: "3", Start: "10", End: "12" },
-        { CourseName: "毛泽东思想和中国特色社会主义理论体系概论", Teacher: "李梓烽", Room: "阶梯B211室", Weeks: "1-1", Day: "4", Start: "1", End: "2" },
-        { CourseName: "毛泽东思想和中国特色社会主义理论体系概论", Teacher: "李梓烽", Room: "阶梯B211室", Weeks: "3-12", Day: "4", Start: "1", End: "2" },
-        { CourseName: "五邑侨乡文化概论(创业精神)", Teacher: "李梓烽", Room: "阶梯B211室", Weeks: "13-18", Day: "4", Start: "1", End: "2" },
-        { CourseName: "教育学", Teacher: "陈龙图", Room: "阶梯B210室", Weeks: "1-18", Day: "4", Start: "3", End: "4" },
-        { CourseName: "高职实用英语(文科)", Teacher: "黄启汉", Room: "教B201", Weeks: "1-10", Day: "4", Start: "5", End: "6" },
-        { CourseName: "高职实用英语(文科)", Teacher: "黄启汉", Room: "教B201", Weeks: "11-11", Day: "4", Start: "6", End: "6" },
-        { CourseName: "体育与健康", Teacher: "马林图", Room: "篮球场1", Weeks: "1-18", Day: "4", Start: "7", End: "8" },
-        { CourseName: "计算机应用基础", Teacher: "叶振安", Room: "图403(电脑15室)", Weeks: "1-18", Day: "5", Start: "1", End: "2" },
-        { CourseName: "形势与政策", Teacher: "谢进伟", Room: "阶梯B210室", Weeks: "12-13", Day: "5", Start: "3", End: "4" },
-        { CourseName: "中国古代文学", Teacher: "王洪生", Room: "教B203", Weeks: "1-18", Day: "5", Start: "5", End: "6" },
-        { CourseName: "毛泽东思想和中国特色社会主义理论体系概论", Teacher: "李梓烽", Room: "没教室", Weeks: "20-20", Day: "7", Start: "1", End: "12" }
-      ]
-    }, function (res) {
-      BmobUser.add('Classes',{
-        Name:"16语文教育1班",
-        CoursesId:res.id
-      },function (res){})
-    })
-
     // 初始化
     setUserInfo(this)
     openNav(this)
@@ -161,103 +111,67 @@ function previewImage(imageUrl) {
 }
 
 
-// //参数为所有课程,本周周数,星期几
-// //筛选今日课程并同步到本地
+//参数为所有课程,本周周数,星期几
+//筛选今日课程并同步到本地
 // function syncTodayCourses(allCourses, week, day) {
-//     setTimeout(function () {
-//         var tmpCourses = []
-//         for (var i = 0; i < allCourses.length; i++) {
-//             var weeks = allCourses[i].weeks.split("-")
-//             if (((allCourses[i]).day == day + "") && ((weeks[0] <= week) && (week <= weeks[1]))) {
-//                 tmpCourses.push(allCourses[i])
-//             }
-//         }
-//         wx.setStorageSync('todayCourses', tmpCourses)
-//     }, 500)
-// }
-
-// //返回距离上次同步时间的天数，之前没有同步则返回-1
-// function getLastSyncTime(syncName) {
-//     let i = wx.getStorageSync(syncName)
-//     if ((i + "") == "") {
-//         return -1
-//     } else {
-//         return (new Date() - i) / (10000 * 60 * 60 * 24)
+//   setTimeout(function () {
+//     var tmpCourses = []
+//     for (var i = 0; i < allCourses.length; i++) {
+//       var weeks = allCourses[i].weeks.split("-")
+//       if (((allCourses[i]).day == day + "") && ((weeks[0] <= week) && (week <= weeks[1]))) {
+//         tmpCourses.push(allCourses[i])
+//       }
 //     }
+//     wx.setStorageSync('todayCourses', tmpCourses)
+//   }, 500)
 // }
 
+// 添加Note并保存id到classes.NotesId
+function addNote(ClassName, CourseName, Title, Content) {
+  BmobUser.add('Notes', {
+    Data: {
+      CourseName: CourseName,
+      Title: Title,
+      Content: Content
+    }
+  }, function (res) {
+    BmobUser.getAll('Classes', function (res2) {
+      for (var i = 0; i < res2.length; i++) {
+        var data = res2[i].attributes;
+        if (data.Name == ClassName) {
+          var tempNotesId = data.NotesId;
+          tempNotesId.push(res.id);
+          BmobUser.updataById('Classes', data.id, {
+            NotesId: tempNotesId
+          });
+        }
+      }
+    });
+  });
+}
 
-// //返回一个Bmob云数据库连接对象
-// function bmob(TableName, type) {
-//     var Bmob = require('../../utils/bmob.js')
-//     if (type == 'add') {
-//         var tmp = Bmob.Object.extend(TableName)
-//         return new tmp()
-//     } else if (type == 'find') {
-//         return new Bmob.Query(Bmob.Object.extend(TableName))
-//     }
-// }
+// 查找16语文教育1班的所有课程并打印
+function getAllCoursesByClassName(ClassName, Cb) {
+  BmobUser.getAll('Classes', function (res) {
+    for (var i = 0; i < res.length; i++) {
+      var data = res[i].attributes;
+      if (data.Name == ClassName) {
+        BmobUser.getOneById('Courses', data.CoursesId, function (res2) {
+          Cb(res2.attributes);
+        });
+      }
+    }
+  });
+}
 
-// //同步班别课程列表
-// //并记录同步时间
-// function syncClassCourses() {
-//     sync('ClassCourse', 'classCourses', 'syncClassCoursesBeforeTime')
-// }
-
-// //获取课程通知
-// //并记录同步时间
-// function syncCourseNotes() {
-//     sync('CourseNote', 'courseNotes', 'syncCourseNotesBeforeTime')
-// }
-
-// //从云数据库获取数据
-// //并记录同步时间
-// function sync(TableName, syncName, syncTime) {
-//     var tmp = bmob(TableName, 'find')
-//     tmp.find({
-//         success: function (results) {
-//             wx.setStorageSync(syncName, results)
-//             wx.setStorageSync(syncTime, new Date())
-//         },
-//         error: function (error) {
-//             alert("查询失败: " + error.code + " " + error.message);
-//         }
-//     });
-// }
-
-
-// // 添加课程通知
-// function addCourseNote(className, content) {
-//     var courseNote = bmob("CourseNote", 'add')
-//     courseNote.set("data", {
-//         "className": className,
-//         "content": content
-//     });
-//     //添加数据，第一个入口参数是null
-//     courseNote.save(null, {
-//         success: function (result) {
-//             // 添加成功，返回成功之后的objectId（注意：返回的属性名字是id，不是objectId），你还可以在Bmob的Web管理后台看到对应的数据
-//             console.log("创建成功, objectId:" + result.id);
-//         },
-//         error: function (result, error) {
-//             // 添加失败
-//             console.log('创建失败');
-
-//         }
-//     });
-// }
-
-// //删除
-// function deleteCourseNote(courseNoteId) {
-//     var courseNote = new bmob("CourseNote", 'find')
-//     courseNote.get(courseNoteId);
-//     courseNote.destroy({
-//         success: function (myObject) {
-//             // 删除成功
-//         },
-//         error: function (myObject, error) {
-//             // 删除失败
-//         }
-//     });
-// }
-
+// 添加课程表数据到Courses并添加id到Classes.CoursesId
+function addCourse(Array, ClassName) {
+  BmobUser.add('Courses', {
+    Data: Array
+  }, function (res) {
+    BmobUser.add('Classes', {
+      Name: ClassName,
+      CoursesId: res.id
+    }, function (res) { })
+  })
+}
